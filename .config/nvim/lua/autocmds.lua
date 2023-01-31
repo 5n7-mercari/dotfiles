68a3562b762665fb1463f45b5d4cdf921e969bbe
Shunta Komatsu <s-komatsu@mercari.com>
2023-01-31 09:35:46 +0900

    feat(nvim): add ts-parser config for cue lang

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,4 +1,9 @@
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.cue" },
+  command = "set filetype=cue",
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { ".envrc" },
   command = "set filetype=sh",
 })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -863,9 +863,20 @@ return require("lazy").setup({
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.cue = {
+        install_info = {
+          url = "https://github.com/eonpatapon/tree-sitter-cue",
+          files = { "src/parser.c", "src/scanner.c" },
+          branch = "main",
+        },
+        filetype = "cue",
+      }
+
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
           "bash",
+          "cue",
           "dockerfile",
           "go",
           "gomod",
